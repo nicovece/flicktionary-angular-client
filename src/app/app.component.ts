250b4a7c6fbeee83183e6597e862135488be7970
Nico Vece <me@nicovece.com>
2025-05-31 17:27:44 +0200

    Feat: Implements app navigation bar
    Adds a navigation bar component to the application.
    
    The navigation bar provides links to different sections of the application such as movies and user profile, and displays a logout button when the user is logged in.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbarComponent],
 })
 export class AppComponent {
   title = 'flicktionary-angular-client';
